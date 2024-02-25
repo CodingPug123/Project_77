ba8d6b34abfb22d8c852c5504f4d947099069d79
CodingPug123 <140002716+CodingPug123@users.noreply.github.com>
2024-02-25 18:28:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var first_name = "Myri ";
+var last_name = "Rao";
+console.log(first_name);
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 20;
+var b = 10;
+var c = a+b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var length_of_first_name = first_name.length;
+var length_of_last_name = last_name.length;
+var length_of_full_name = full_name.length;
+
+console.log(length_of_first_name);
+console.log(length_of_last_name);
+console.log(length_of_full_name);
+
+var d = Math.pow(a, b);
+var e = Math.round(18.4839);
+var f = Math.round(48.7293);
+
+console.log(d);
+console.log(e);
+console.log(f);
+
+var message_for_teacher = "You are the best coding teacher ever! Ever since I started coding with you, I improved so much! You helping me become a better coder. Thank you so much!";
+
+console.log(message_for_teacher);
